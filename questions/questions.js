ea716378d6aaa9f07bfb02a1ac4f2b7d40e29e1e
rmezied <rmezied3@gmail.com>
2021-06-30 23:33:07 -0700

    update js #6

--- questions/questions.js
+++ questions/questions.js
@@ -1,4 +1,27 @@
 let score = 0  
+let questionNum = 1
+
+function createQuestsionSection(q){
+    document.querySelector(".question-section").innerHTML = "";
+    let questionBox = document.createElement("p")
+    questionBox.setAttribute('class',"question")
+    let questionText = document.createTextNode(q);
+    questionBox.appendChild(questionText);
+    document.querySelector(".question-section").appendChild(questionBox);
+}
+
+function createChoicesSection(c){
+    document.querySelector(".choices-section").innerHTML = "";
+    qChoices.forEach(ele=>{
+        let choiceBox = document.createElement("button")
+        choiceBox.setAttribute('class','choice')
+           
+        let choiceText = document.createTextNode(ele);
+        choiceBox.appendChild(choiceText);
+        document.querySelector(".choices-section").appendChild(choiceBox);
+    })
+}
+
 function addQuestion(){
     fetch(`https://opentdb.com/api.php?amount=50&type=multiple`)
     .then(response => {return response.json()})
@@ -8,21 +31,25 @@ function addQuestion(){
         correctChoice = data.results[r]["correct_answer"]
         qChoices = (data.results[r]["incorrect_answers"])
         qChoices.push(correctChoice)
-        let questionBox = document.createElement("p")
-        questionBox.setAttribute('class',"question")
-        let questionText = document.createTextNode(randomQ);
-        questionBox.appendChild(questionText);
-        document.querySelector(".question-section").appendChild(questionBox);
-        qChoices.forEach(ele=>{
-            let choiceBox = document.createElement("p")
-            choiceBox.setAttribute('class','choice')
-            let choiceText = document.createTextNode(ele);
-        choiceBox.appendChild(choiceText);
-        document.querySelector(".choices-section").appendChild(choiceBox);
-        })
+        createQuestsionSection(randomQ)
+        createChoicesSection(qChoices)      
     })
 }
 addQuestion()
-document.querySelector("#next-button").addEventListener('click', addQuestion)
+document.querySelector("#next-button").addEventListener('click', moveToNext)
+function moveToNext(){
+    let x = document.querySelectorAll("choiceCheck").values
+    console.log(x)
+    // x.forEach(element => {
+    //     console.log(element.textContent)        
+    // });
+    
+
+    // }
+    // else{
+    //     countScore()
+    //     addQuestion()
+    // }
+}
 
         
